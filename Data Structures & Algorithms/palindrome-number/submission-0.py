93d65c531159b38251174b704d4d44a3bfcfbeb9
Manish Chaudhari <98401661+ManishChaudhari-11@users.noreply.github.com>
2026-07-15 00:58:34 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num = abs(x)
+        rev = 0
+
+        while num > 0:
+            rev = rev * 10 + num % 10
+            num //= 10
+        
+        if rev <= (-2**31) and rev >= (2**31 -1):
+            return False
+        
+        return rev == x
